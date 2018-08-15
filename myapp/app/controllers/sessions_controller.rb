5ec6abe88c504d2684fde1075c00f1e5cb8c274c
Xtail <xtail1996@yandex.ru>
2018-08-15 17:52:54 +0300

    upd

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = user.id
+    tenant = Tenant.from_omniauth(request.env["omniauth.auth"])
+    session[:user_id] = tenant.id
     redirect_to root_url
   end
 
